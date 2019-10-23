b9a4095e4baab0b1a7e184f63c4a04cb71650569
davidfung <davidfung@amgcomputing.com>
2019-10-23 15:38:57 -0700

    Before removing non-relevant code

--- app/src/main/java/com/example/appwidgetsample/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/appwidgetsample/AlarmBroadcastReceiver.java
@@ -8,16 +8,31 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        // get the layout for the widget and update the battery percentage
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                 R.layout.new_app_widget);
-        remoteViews.setTextViewText(R.id.appwidget_id, "ABC");
+        ////remoteViews.setTextViewText(R.id.appwidget_id, "CAT");
+        // Compile the last update time string
+        String dateString =
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+        String lastUpdateString = context.getResources().getString(
+                R.string.date_count_format, 0, dateString);
+        ////remoteViews.setTextViewText(R.id.button_update, lastUpdateString);
+        remoteViews.setTextViewText(R.id.appwidget_id, lastUpdateString);
+
+        Log.d("AMG99", "Alarm onReceive() " + lastUpdateString);
+
+        // Tell the AppWidgetManager to perform an update on the widget
         ComponentName thisWidget = new ComponentName(context, NewAppWidget.class);
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
         manager.updateAppWidget(thisWidget, remoteViews);
-        Log.d("XXXXXXXXXXXXXX", "onReceive: ABC");
     }
 }
--- app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -9,12 +9,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.BatteryManager;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.text.DateFormat;
 import java.util.Date;
-
-import static java.lang.Math.round;
 
 /**
  * Implementation of App Widget functionality.
@@ -24,62 +23,70 @@ public class NewAppWidget extends AppWidgetProvider {
     private static final String mSharedPrefFile = "com.example.android.appwidgetsample";
     private static final String COUNT_KEY = "count";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+    static void updateWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+         // Increment the count
         SharedPreferences prefs = context.getSharedPreferences(
                 mSharedPrefFile, 0);
         int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
         count++;
 
-        String dateString =
-                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
-
-        views.setTextViewText(R.id.appwidget_update,
-                context.getResources().getString(
-                        R.string.date_count_format, count, dateString));
-
-        // override the widget id with battery level
-        int batteryLevel = getBatteryLevelPct(context);
-        views.setTextViewText(R.id.appwidget_id, batteryLevel + "%");
-
+        // Save the count
         SharedPreferences.Editor prefEditor = prefs.edit();
         prefEditor.putInt(COUNT_KEY + appWidgetId, count);
         prefEditor.apply();
 
-        // Setup update button to send an update request as a pending intent.
-        Intent intentUpdate = new Intent(context, NewAppWidget.class);
+        // Compile the last update time string
+        String dateString =
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+        String lastUpdateString = context.getResources().getString(
+                R.string.date_count_format, count, dateString);
 
-        // The intent action must be an app widget update.
-        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        // Construct the RemoteViews object and update the content
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+        views.setTextViewText(R.id.appwidget_update, lastUpdateString);
 
-        // Include the widget ID to be updated as an intent extra.
-        int[] idArray = new int[]{appWidgetId};
-        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        Log.d("AMG99", "updateAppWidget() " + lastUpdateString);
 
-        // Wrap it all in a pending intent to send a broadcast.
-        // Use the app widget ID as the request code (third argument) so that
-        // each intent is unique.
-        PendingIntent pendingUpdate = PendingIntent.getBroadcast(context,
-                appWidgetId, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Assign the pending intent to the button onClick handler
-        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
+        // override the widget id with battery level
+        int batteryPct = getBatteryLevelPct(context);
+        if (batteryPct != (-1)) {
+            views.setTextViewText(R.id.appwidget_id, batteryPct + "%");
+        }
+        Log.d("AMG99", "updateAppWidget() " + batteryPct + "%");
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        // Setup update button to send an update request as a pending intent.
+        // The intent action must be an app widget update.
+        // Include the widget ID to be updated as an intent extra.
+        // Wrap it all in a pending intent to send a broadcast.
+        // Use the app widget ID as the request code (third argument) so that
+        // each intent is unique.
+        // Assign the pending intent to the button onClick handler
+        Intent intent = new Intent(context, NewAppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId};
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        PendingIntent pi = PendingIntent.getBroadcast(context,
+                appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.button_update, pi);
     }
 
-    static int getBatteryLevelPct(Context context){
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = context.registerReceiver(null, ifilter);
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        int batteryPct = Math.round(level / (float)scale * 100);
+    static int getBatteryLevelPct(Context context) {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, filter);
+        int level;
+        int scale;
+        int batteryPct = (-1);
+        if (batteryStatus != null) {
+            level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            batteryPct = Math.round(level / (float) scale * 100);
+        }
         return batteryPct;
     }
 
@@ -87,17 +94,21 @@ public class NewAppWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        // Create an Intent to update the widget
         Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
-        am.setRepeating(AlarmManager.RTC, System.currentTimeMillis()+ 100 * 3, 60000 , pi);
+
+        // Set a repeating alarm to call the pending intent
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC, System.currentTimeMillis() + 100 * 3, 60000 * 10, pi);
     }
 
 }
